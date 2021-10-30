ef697f60382c70760534398ebe5b8db4bf2164fb
Bandgar09 <jotiram.bandgar@biomarking.com>
2021-10-30 16:36:22 +0530

    Added functionality to add two numbers

--- src/com/bridgelabz/HelloWorld.java
+++ src/com/bridgelabz/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 
 public class HelloWorld {
+    public static int addNum(int num1, int num2){
+        return num1 + num2;
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int addResult = addNum(3, 2);
+        System.out.println("addition result--->" + addResult);
     }
 }
